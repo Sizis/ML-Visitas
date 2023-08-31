92f05424c8901610bb9a40f4f366e0f24ebeccbf
Jon <jiovelezsalas@gmail.com>
2023-08-31 14:57:38 +0200

    Predicciones 2

--- predicciones.py
+++ predicciones.py
@@ -6,11 +6,11 @@ fit_model = model.fit(damping_slope=0.8)
 # Solicitar al usuario ingresar una fecha futura
 while True:
     try:
-        input_date = input("Ingresa una fecha futura en formato YYYY-DD-MM: ")
+        input_date = input("Ingresa una fecha futura en formato YYYY-MM-DD: ")
         forecast_date = pd.to_datetime(input_date)
         break
     except ValueError:
-        print("Por favor, ingresa una fecha válida en el formato correcto.")
+        print("Por favor, ingresa una fecha válida en el formato correcto YYYY-MM-DD.")
 
 # Solicitar al usuario elegir la opción de pronóstico
 while True:
@@ -28,17 +28,26 @@ if forecast_option.lower() == 'semana':
 else:
     forecast_steps = 30  # Asumiendo un mes de 30 días
 
+# LLamar a la base de datos y sacar última fecha
+# cargar los datos de la db
+conn = sqlite3.connect('DB/daily_visits.db')
+df = pd.read_sql("SELECT * FROM visit_quant", conn, parse_dates='Date', index_col='Date')
+conn.close()
+
+# Obtener la última fecha del DataFrame
+last_date = df.index.max()
+
 # Realizar el pronóstico para la fecha ingresada y la cantidad de pasos
-forecast = fit_model.forecast(steps=forecast_steps)
+forecast = fit_model.predict(forecast_steps + (forecast_date - last_date).days)
 forecast_values = forecast[:forecast_steps].astype(int)
 
-print(f"Predicción de visitas para la fecha {forecast_date.date()}:")
-for i in range(forecast_steps):
-    print(f"{i+1} {forecast_values[i]}")
-
-    # Crear un DataFrame con los resultados
+# Crear un DataFrame con los resultados
 forecast_df = pd.DataFrame({'Fecha': pd.date_range(start=forecast_date, periods=forecast_steps),
                             'Predicción': forecast_values})
+
+# Restablecer el índice y convertir la columna "Fecha" en el nuevo índice
+forecast_df.reset_index(drop=True, inplace=True)
+forecast_df.set_index('Fecha', inplace=True)
 
 # Mostrar el DataFrame de resultados
 print("\nResultados de la Predicción:")
